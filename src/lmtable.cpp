7e3885683ea48e55e436089431b6a65dab0d6526
nicolabertoldi <nicolabertoldi@ee1e936f-7723-0410-939f-cd31102c90e6>
2011-07-28 10:01:36 +0000

    I fixed a bug affecting the computation of prob with quantized LM and inverted n-grams; I fixed a bug affecting the textual writing of the LM with inverted n-grams; I also did some clean up

--- src/lmtable.cpp
+++ src/lmtable.cpp
@@ -1146,7 +1146,7 @@ lmtable* lmtable::cpsublm(dictionary* subdict,bool keepunigr){
     }
   }
 	
-  //mange dictionary information
+  //manage dictionary information
 	
   //generate OOV codes and build dictionary lookup table 
   dict->genoovcode(); slmt->dict->genoovcode(); subdict->genoovcode();
@@ -1791,7 +1791,7 @@ void lmtable::dumplm(fstream& out,ngram ng, int ilev, int elev, table_entry_pos_
   int ndsz=nodesize(ndt);
 	
   assert(ng.size==ilev-1);
-  //Note that ipos and epos are always large r tahn or equal to 0 because they are unsigned int
+  //Note that ipos and epos are always larger than or equal to 0 because they are unsigned int
   assert(epos<=cursize[ilev] && ipos<epos);
   ng.pushc(0);
 	
@@ -1799,7 +1799,6 @@ void lmtable::dumplm(fstream& out,ngram ng, int ilev, int elev, table_entry_pos_
     *ng.wordp(1)=word(table[ilev]+(table_pos_t) i*ndsz);
 		
     float ipr=prob(table[ilev]+(table_pos_t) i*ndsz,ndt);
-    //int ipr=prob(table[ilev]+i*ndsz,ndt);
 		
     //skip pruned n-grams
     if(isPruned && ipr==NOPROB) continue;
@@ -1814,32 +1813,26 @@ void lmtable::dumplm(fstream& out,ngram ng, int ilev, int elev, table_entry_pos_
       //cout << "no successors for " << ng << "\n";
     }
     else{
-      //out << i << " "; //this was just to count printed n-grams
       out << ipr <<"\t";
-      //out << (isQtable?ipr:*(float *)&ipr) <<"\t";
 			
       // if table is inverted then revert n-gram
       if (isInverted & ng.size>1){
 	ing.invert(ng);
-	ng=ing;
-      }
-			
-      for (int k=ng.size;k>=1;k--){
-	if (k<ng.size) out << " ";
-	out << lmtable::getDict()->decode(*ng.wordp(k));
+        for (int k=ing.size;k>=1;k--){
+          if (k<ing.size) out << " ";
+          out << lmtable::getDict()->decode(*ing.wordp(k));
+        }
+      }else{
+        for (int k=ng.size;k>=1;k--){
+	  if (k<ng.size) out << " ";
+	  out << lmtable::getDict()->decode(*ng.wordp(k));
+        }
       }
 			
       if (ilev<maxlev){
 	float ibo=bow(table[ilev]+ (table_pos_t)i * ndsz,ndt);
 	if (isQtable) out << "\t" << ibo;
 	else if (ibo!=0.0) out << "\t" << ibo;
-	/*
-	  int ibo=bow(table[ilev]+ i * ndsz,ndt);
-	  if (isQtable) out << "\t" << ibo;
-	  else
-	  if (*((float *)&ibo)!=0.0)
-	  out << "\t" << *((float *)&ibo);
-	*/
       }
       out << "\n";
     }
@@ -2004,11 +1997,10 @@ double lmtable::lprob(ngram ong,double* bow, int* bol, char** maxsuffptr,unsigne
     ngram ing=ong; //inverted ngram
 		
     ing.invert(ong);
-    //cout << "ngram:" << ing << "\n";	
     get(ing,ing.size,ing.size); // dig in the trie
     if (ing.lev >0){ //found something?
       iprob=ing.prob;
-      lpr = (double)(isQtable?Pcenters[ing.size][(qfloat_t)iprob]:iprob);
+      lpr = (double)(isQtable?Pcenters[ing.lev][(qfloat_t)iprob]:iprob);
       if (*ong.wordp(1)==dict->oovcode()) lpr-=logOOVpenalty; //add OOV penalty
       if (statesize)  *statesize=MIN(ing.lev,(ing.size-1)); //find largest n-1 gram suffix 
       if (maxsuffptr) *maxsuffptr=ing.path[MIN(ing.lev,(ing.size-1))];			
