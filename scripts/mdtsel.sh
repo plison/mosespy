e346bb1424114c6829bb18a32073442ac6b832a3
mfederico <mfederico@ee1e936f-7723-0410-939f-cd31102c90e6>
2012-09-19 13:52:55 +0000

    Fixed a silly error

--- scripts/mdtsel.sh
+++ scripts/mdtsel.sh
@@ -1,30 +1,30 @@
 #! /bin/bash 
 
-/******************************************************************************
-IrstLM: IRST Language Model Toolkit
-Copyright (C) 2006 Marcello Federico, ITC-irst Trento, Italy
+#/******************************************************************************
+#IrstLM: IRST Language Model Toolkit
+#Copyright (C) 2006 Marcello Federico, ITC-irst Trento, Italy
+#
+#This library is free software; you can redistribute it and/or
+#modify it under the terms of the GNU Lesser General Public
+#License as published by the Free Software Foundation; either
+#version 2.1 of the License, or (at your option) any later version.
+#
+#This library is distributed in the hope that it will be useful,
+# 
+# 
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#Lesser General Public License for more details.
+#
+#You should have received a copy of the GNU Lesser General Public
+#License along with this library; if not, write to the Free Software
+#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+#******************************************************************************/
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
- 
- 
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
-
-******************************************************************************/
-
-// dtsel
-// by M. Federico
-// Copyright Marcello Federico, Fondazione Bruno Kessler, 2012
+# mdtsel.sh
+# by M. Federico
+# Copyright Marcello Federico, Fondazione Bruno Kessler, 2012
 
 
 set -m #enable job control
