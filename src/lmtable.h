0ee87e0a43c5904feccfa07b95967e4408f04394
nicolabertoldi <nicolabertoldi@ee1e936f-7723-0410-939f-cd31102c90e6>
2014-05-12 13:34:11 +0000

    code cleanup

--- src/lmtable.h
+++ src/lmtable.h
@@ -63,7 +63,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 #define UNIGRAM_RESOLUTION 10000000.0
 
 typedef enum {INTERNAL,QINTERNAL,LEAF,QLEAF} LMT_TYPE;
-//typedef enum {BINARY,TEXT,YRANIB,NONE} OUTFILE_TYPE;
 typedef char* node;
 
 typedef enum {LMT_FIND,    //!< search: find an entry
