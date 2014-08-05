b1997f725d44f15cf151a78ebb0a7d4223167bc5
plison2 <plison2@gmail.com>
2014-08-05 19:36:23 +0000

    adding init file again

--- run_experiment.py
+++ run_experiment.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*- 
 
 import sys
-from mosespy import shellutils
-from mosespy import slurm
 from mosespy.slurm import SlurmExperiment
 from mosespy.moseswrapper import Experiment
 
 trainData = "./data/news-commentary/news-commentary-v8.fr-en";
 tuningData = "./data/newstest/newssyscomb2009"
+testData = "./data/newstest/newstest2011"
 
 exp = SlurmExperiment("basic", "fr", "en")
-exp.trainLanguageModel(trainData+".en")
-exp.trainTranslationModel(trainData, nbSplits=4)
-exp.tuneTranslationModel(tuningData)
-exp.binariseModel()
-print exp.translate("Faire revenir les militants sur le terrain et convaincre que le vote est utile.")
+#exp.trainLanguageModel(trainData+".en")
+#exp.trainTranslationModel(trainData, nbSplits=4)
+#exp.tuneTranslationModel(tuningData)
+#exp.binariseModel()
+#print exp.translate("Faire revenir les militants sur le terrain et convaincre que le vote est utile.")
+exp.evaluateBLEU(testData)
