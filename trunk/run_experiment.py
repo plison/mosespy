0e3f14c38e4134da4e81d9f5affdf0be8a1487d3
plison2@gmail.com <plison2@gmail.com@aaf2fdce-b361-1a6b-0cb7-b2236dfd8bc3>
2014-10-03 21:26:48 +0000

    dictionary files

--- trunk/run_experiment.py
+++ trunk/run_experiment.py
@@ -1,14 +1,3 @@
 # -*- coding: utf-8 -*- 
 
-from mosespy.experiment import Experiment
-
-trainData = "./data/news-commentary/news-commentary-v8.fr-en"
-tuningData = "./data/newstest/news-test2008"
-testData = "./data/newstest/newstest2011"
-
-exp = Experiment("first_test", "fr", "en")
-exp.trainLanguageModel(trainData+".en")
-exp.trainTranslationModel(trainData)
-exp.tuneTranslationModel(tuningData)
-exp.evaluateBLEU(testData)
-
+from mosespy.analyser import AlignmentList
